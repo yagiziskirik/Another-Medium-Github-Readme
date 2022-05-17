60a4cfaace717d164ae4c74a35a565546a485e5c
yagiziskirik <y_iskirik_10@hotmail.com>
2022-05-17 16:12:55 +0300

    Fix image not showing

--- api/medium.js
+++ api/medium.js
@@ -1,6 +1,6 @@
 const request = require('request')
 import moment from 'moment';
-const imageToBase64 = require('image-to-base64')
+const axios = require('axios')
 
 function createImage(title, pubDate, link, author, base64ImageLink, description, descLength, titleColor, authorColor, descColor, bgColor, dateColor, highlightConvertedColor, borderRadius, borderColor, generalWidth) {
   var shortDescription = description.replace(/<\/?[^>]+(>|$)/g, '').replace('\n', ' ').substr(0,descLength) + '...'
@@ -120,7 +120,7 @@ module.exports = (req, res) => {
   const highlightColor = req.query.highlightColor || '2e2e2e'
   const generalWidth = req.query.generalWidth || '800'
   const highlightConvertedColor = hexToRGBA(highlightColor)
-	request('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@'+username, { json: true }, (err, resp, body) => {
+	request('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@'+username, { json: true }, async (err, resp, body) => {
     if (err) { return console.log(err) }
     var title = body.items[index].title
     var pubDate = body.items[index].pubDate
@@ -128,19 +128,12 @@ module.exports = (req, res) => {
     var author = body.items[index].author
     var thumbnail = body.items[index].thumbnail
     var description = body.items[index].description
-    imageToBase64(thumbnail) // Image URL
-    .then(
-      (response) => {
-        console.log(response)
-        var svgImage = createImage(title, pubDate, link, author, response, description, descLength, titleColor, authorColor, descColor, bgColor, dateColor, highlightConvertedColor, borderRadius, borderColor, generalWidth)
-		    res.setHeader("Content-Type","image/svg+xml")
-		    res.status(200).send(svgImage)
-      }
-    )
-    .catch(
-      (error) => {
-        console.log(error)
-      }
-    )
+    const { data: thumbnailRaw } = await axios.get(thumbnail, {
+      responseType: 'arraybuffer',
+    })
+    const base64Img = Buffer.from(thumbnailRaw).toString('base64')
+    var svgImage = createImage(title, pubDate, link, author, base64Img, description, descLength, titleColor, authorColor, descColor, bgColor, dateColor, highlightConvertedColor, borderRadius, borderColor, generalWidth)
+    res.setHeader("Content-Type","image/svg+xml")
+    res.status(200).send(svgImage)
 	})
 }
